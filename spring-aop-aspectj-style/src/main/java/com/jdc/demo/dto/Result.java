6a630e279bff347baa088ed21ef7af2fd9d75a8a
MoeMinThway <moeminthway.mink@gmail.com>
2023-11-24 22:06:17 +0630

    Initial commit

---
+++ spring-aop-aspectj-style/src/main/java/com/jdc/demo/dto/Result.java
@@ -0,0 +1,9 @@
+package com.jdc.demo.dto;
+
+public record Result(
+		String name,
+		int count) {
+
+
+
+}
---
+++ spring-aop-aspectj-style/src/main/java/com/jdc/demo/service/MyService.java
@@ -0,0 +1,21 @@
+package com.jdc.demo.service;
+
+import org.springframework.stereotype.Component;
+
+import com.jdc.demo.dto.Result;
+
+@Component
+public class MyService {
+	
+//	public void hello(String name,int count) {
+//		System.out.println("Hello from MyService");
+//	}
+	
+	public Result hello(String name,int count) {
+		System.out.println("Hello from MyService");
+		return new Result(name,count);
+	}
+	public int divided(int a,int b) {
+		return a / b;
+	}
+}
